45e048be232586b9dfde72c9562977238360b5d5
Fabio Henrique <fabio.henrique100@hotmail.com>
2023-10-17 21:36:20 -0300

    Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Form';
+import Lista from './components/List';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Formulario />
+      <Lista />
     </div>
   );
 }
